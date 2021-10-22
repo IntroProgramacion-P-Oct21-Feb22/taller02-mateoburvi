3d213d58f17cde5a71ea05dcebadd0a3a1b829c6
Mateo Burneo <msburneo1@utpl.edu,ec>
2021-10-22 13:51:45 -0500

    primer commit

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author Mateo
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       
+    }
+    
+}
